858c47fff68cd5201d39d1ec1bc59a6636f3daa6
Hendi Santika <hendisantika@yahoo.co.id>
2023-09-09 10:26:33 +0700

    #98 - Upgraded into Spring Boot 2.9.7 version ✏️📗:octocat:⬆
    #98 - Upgraded into Spring Boot 2.9.7 version ✏️📗:octocat:⬆

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.9.6"
+	id("org.springframework.boot") version "2.9.7"
 	id("io.spring.dependency-management") version "1.0.8.RELEASE"
 	kotlin("jvm") version "1.3.50"
 	kotlin("plugin.spring") version "1.3.50"
