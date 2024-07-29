91bb245e187b4cc0cf1ed9bc28cbe2850799a60f
Hendi Santika <hendisantika@yahoo.co.id>
2024-07-29 10:04:44 +0700

    Upgraded into Spring Boot 3.3.0 version 🫸🌀✏️📗🐧🐳⬆
    Upgraded into Spring Boot 3.3.0 version 🫸🌀✏️📗🐧🐳⬆

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.2.9"
+	id("org.springframework.boot") version "3.3.0"
 	id("io.spring.dependency-management") version "1.0.8.RELEASE"
 	kotlin("jvm") version "1.3.50"
 	kotlin("plugin.spring") version "1.3.50"
@@ -10,7 +10,7 @@ plugins {
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 repositories {
 	mavenCentral()
